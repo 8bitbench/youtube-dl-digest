e7d0dfac700ae33f1b71c882128f75fdca9392ba
Jacob <8bitbench@users.noreply.github.com>
2019-03-09 13:49:14 -0800

    making sure the youtube-dl stays current

--- files/bin/youtube-dl_orchestrator.sh
+++ files/bin/youtube-dl_orchestrator.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 ps aux |grep youtube-dl_script.sh |grep -v grep |wc -l > /toolbox/app/running.bit
-#!/bin/bash
+youtubeDL=$(which youtube-dl)
 sleep 1;
 if [ `cat /toolbox/app/running.bit` -eq "0" ]
 then
+$youtubeDL -U >out 2>&1 >/dev/null
 /toolbox/app/bin/youtube-dl_script.sh /toolbox/app/conf/dl_sources.csv /toolbox/data
 fi
